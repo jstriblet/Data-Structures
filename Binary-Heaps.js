9ddcadc4d9a0017117894ccd849d7e5c7d99ee5b
Jonathan Striblet <jstriblet@gmail.com>
2019-08-31 18:08:37 -0700

    added Binary Heap

--- Binary-Heaps.js
+++ Binary-Heaps.js
@@ -3,12 +3,47 @@
 // MinBinaryHeap, parent nodes are always smaller than their chil nodes
 // Each parent has at most two nodes
 // A binary heap is as compact as possible with the left childresn filled out first
+// Binary Heaps are used to implement Priority Queues, they are also used quite a bit
+// with graph travertal algorithms
+// When represented as an array, 
+// the children of any node can be found at the index of (2n+1 & 2n+2)
 
-class Node {
-    constructor (val){
-        this.val = val;
-        this.left = null;
-        this.right = null;
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+    insert(val) {
+        this.values.push(val);
+        const swap = (arr, i, j) => {
+            const tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        } 
+
+        let curr = this.values.length - 1
+        let prnt = Math.floor((curr - 1) / 2 );
+
+        while (this.values[curr] > this.values[prnt] && prnt >= 0) {
+            swap(this.values, curr, prnt)
+            curr = prnt
+            prnt = Math.floor((curr-1)/2)
+        }
+        return this.values
     }
 }
+
+let heap = new MaxBinaryHeap()
+
+console.log(heap.insert(20));
+console.log(heap.insert(50));
+console.log(heap.insert(33));
+console.log(heap.insert(47));
+console.log(heap.insert(12));
+console.log(heap.insert(55));
+console.log(heap.insert(15));
+console.log(heap.insert(10));
+console.log(heap.insert(100));
+console.log(heap.insert(1));
+
+
 
